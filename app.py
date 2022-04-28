86b6a06941e0d43733f211c0918d4ae05b4917c7
RonShan03 <ronshan248@gmail.com>
2022-04-27 17:45:52 -0700

    added files

--- app.py
+++ app.py
@@ -2,13 +2,14 @@
 from flask import Flask, render_template
 from data import get_data
 import random
+import os
 
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 @app.route('/')
 def hello():
     '''This function is the main function for the application.'''
-    info = get_data()
 
     '''Dua Lipa, Olivia Rodrigo, Ariana Grande'''
     artist_list = ['6M2wZ9GZgrQXHCFfjv46we', '1McMsnEElThX1knmY4oliG', '66CXWjxzNUsdJxJ2JdwvnR']
@@ -19,3 +20,18 @@ def hello():
     print(info)
 
     return render_template(
+        'index.html',
+        song_name = info[0],
+        artist_names = info[1],
+        album_image = info[2],
+        song_preview = info[3],
+        spotify_link = info[4],
+        total_artists = len(info[1])
+    )
+
+if __name__ == '__main__':
+    app.run(
+        debug=True,
+        host=os.getenv('IP', '0.0.0.0'),
+        port=int(os.getenv('PORT', 8080))
+    )
--- data.py
+++ data.py
@@ -1,7 +1,9 @@
+'''This file gets the data from the Spotify API'''
+import random
+import requests
 from auth import get_auth
 
 
-def get_data():
 def get_data(artist_id):
     '''This function gets the data from the Spotify API.'''
     access_token = get_auth()
@@ -9,15 +11,30 @@ def get_data(artist_id):
         'Authorization': 'Bearer {TOKEN}'.format(TOKEN=access_token)
     }
 
-    URL = 'https://api.spotify.com/v1/artists/{id}/top-tracks'.format(id='6M2wZ9GZgrQXHCFfjv46we')
     URL = 'https://api.spotify.com/v1/artists/{id}/top-tracks'.format(id=artist_id)
     data = requests.get(URL + "?market=US", headers = headers)
 
     data = data.json()
 
-    #rand = random.randint(0, len(data['tracks']) - 1)
-    rand = 2
     rand = random.randint(0, len(data['tracks']) - 1)
 
     artist_names = []
     for i in data["tracks"][rand]["artists"]:
+        artist_names.append(i["name"])
+
+
+    song_name = data["tracks"][rand]["name"]
+
+    album_image = data["tracks"][rand]["album"]["images"][0]["url"] # Could also be null
+
+    song_preview = data["tracks"][rand]["preview_url"] # Can be Null
+
+    spotify_link = data["tracks"][rand]["external_urls"]["spotify"]
+
+    info = [song_name, artist_names, album_image, song_preview, spotify_link]
+
+    return info
+
+if __name__ == '__main__':
+    x = get_data()
+    print(x)
